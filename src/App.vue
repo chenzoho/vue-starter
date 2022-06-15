beb24a0f5b79e86cb17ee87711c2d6139a907c4e
陈泽洪 <447188858@qq.com>
2022-06-15 13:33:17 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import CountComp from "./components/Count.vue";
+import TsxDemo from "./components/TsxTest";
+</script>
+
+<template>
+	<img alt="Vue logo" src="./assets/logo.png" />
+	<TsxDemo />
+	<CountComp msg="Vue 3 + TypeScript + Vite + Tsx" />
+</template>
+
+<style>
+#app {
+	margin-top: 60px;
+	font-family: Avenir, Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	color: #2c3e50;
+	text-align: center;
+}
+</style>
